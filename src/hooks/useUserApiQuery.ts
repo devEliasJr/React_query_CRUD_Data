d656e925fc1f022b15893391bf27125d8d1ae6ef
devEliasJr <eliasengmecanica@gmail.com>
2023-09-17 14:23:56 -0300

    feat: UseQuery all data and data by id

--- src/hooks/useUserApiQuery.ts
+++ src/hooks/useUserApiQuery.ts
@@ -1,13 +1,24 @@
 import { useQuery } from "react-query";
-import { getAllData } from "./useFetchData";
+import { getAllData, getUserById } from "./useFetchData";
 
 
-export function useUserData () {
+export function useGetAllUsersQuery () {
   const query = useQuery<IDataProps[], Error>({
     queryFn: getAllData,
     queryKey: ["users"],
     retry: false,
     refetchInterval: 5 * 60 * 1000 // 5 minutes
+  })
+
+  return query 
+}
+
+export function useGetUserById (id: number) {
+  const query = useQuery<IDataProps, Error>({
+    queryFn: () => getUserById(id),
+    queryKey: ["user"],
+    retry: false,
+    enabled: !!id
   })
 
   return query 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import { Box } from "@mui/material";
 import CardsComponents from "../../components/cards";
-import { useUserData } from "../../hooks/useUserApiQuery";
+import { useGetAllUsersQuery } from "../../hooks/useUserApiQuery";
 
 export default function HomePage() {
-  const {data: users, error, isLoading} = useUserData()
+  const {data: users, error, isLoading} = useGetAllUsersQuery()
   return (
     <Box>
       <CardsComponents user={users} error={error} loading={isLoading} />
