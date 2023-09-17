ddddfc4c028604b65735bb61d444d1bc4a8d330d
devEliasJr <eliasengmecanica@gmail.com>
2023-09-17 16:11:21 -0300

    feat: Added Query Context

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,19 @@ import App from "./App.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import darkTheme from "./styles/Dark.ts";
 import { QueryClientProvider, QueryClient } from "react-query";
+import AppQueryProvider from "./Contexts/useQueryContext.tsx";
 
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <AppQueryProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </AppQueryProvider>
   </React.StrictMode>
 );
